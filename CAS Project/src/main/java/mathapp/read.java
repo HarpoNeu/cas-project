b820afc160c023664bafeca356e6c98be5549440
Riley Krzywda <rkrzywda@rkrzywda-mbp.lan>
2020-01-19 14:43:07 +0200

    basic check

--- CAS Project/src/main/java/mathapp/read.java
+++ CAS Project/src/main/java/mathapp/read.java
@@ -3,6 +3,7 @@ package mathapp;
 import java.io.*;
 import java.net.URISyntaxException;
 import java.net.URL;
+import java.util.Scanner;
 
 public class Read {
 
@@ -11,21 +12,36 @@ public class Read {
         URL path = ClassLoader.getSystemResource("Questions.txt");
         File file = new File(path.toURI());
         BufferedReader br = new BufferedReader(new FileReader(file));
-        
+        Scanner input = new Scanner(System.in);
+        System.out.print("Answer: ");
+        String answer = input.nextLine();
         String line; 
         String completeFile = "";
+        Boolean correct = false;
 
         while ((line = br.readLine()) != null) {
              completeFile = completeFile + "\n" + line;
         } 
         String questions[] = completeFile.split(";");
         String qLines[][] = new String[questions.length][];
-
-        for(int a = 0; a < questions.length; a ++)
+        for(int a = 0; a < questions.length; a++)
         {
             qLines[a] = questions[a].split("\n");
+            System.out.println(answer);
+            if(answer.equals("4"))
+            {
+                correct = true;
+                break;
+            }
         }
-        System.out.println(qLines[1][2]);
+        if(correct == false)
+        {
+            System.out.println("Incorrect");
+        }
+        else{
+            System.out.println("Correct");
+        }
+        
 
 
     }
