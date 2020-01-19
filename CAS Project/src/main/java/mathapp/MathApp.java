7ff497e223f32472b1851e20d75bbd8a85ac7f01
Isaac Neumann <ineumann@jneumann-mbp.local>
2020-01-19 10:10:14 +0200

    Button Test

--- CAS Project/src/main/java/mathapp/MathApp.java
+++ CAS Project/src/main/java/mathapp/MathApp.java
@@ -3,15 +3,22 @@ package mathapp;
 import javafx.application.*;
 import javafx.stage.*;
 import javafx.scene.*;
+import javafx.scene.control.*;
 
 public class MathApp extends Application
 {
     public void start(Stage primaryStage)
     {
-        Scene scene = new Scene(new Group());
+        Group grp = new Group();
+
+        Scene scene = new Scene(grp, 800, 600);
+
+        Button btn = new Button("Test Button");
+        grp.getChildren().add(btn);
 
         primaryStage.setScene(scene);
         primaryStage.setTitle("Math App");
+        primaryStage.centerOnScreen();
         primaryStage.show();
     }
 
